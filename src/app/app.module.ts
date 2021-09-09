cdc79ac2e6789ba0fcc8b02822e6ec6ff69200d4
EuronRomanov <juanpablo.gaona@yahoo.com>
2021-09-08 21:53:36 -0500

    Sin dishdetail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatListModule } from "@angular/material/list";
 import { FlexLayoutModule } from '@angular/flex-layout';
+import {  MatGridList, MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import "hammerjs";
 import { MenuComponent } from './menu/menu.component';
+import { DishtailComponent } from './dishtail/dishtail.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishtailComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +26,10 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dishtail/dishtail.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DishtailComponent } from './dishtail.component';
+
+describe('DishtailComponent', () => {
+  let component: DishtailComponent;
+  let fixture: ComponentFixture<DishtailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DishtailComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DishtailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
